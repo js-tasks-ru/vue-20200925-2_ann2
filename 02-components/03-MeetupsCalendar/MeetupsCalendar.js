d3343666459147a43a26fc2526fb54e8f977863b
Anna Podbelskaya <podag@yandex.ru>
2020-10-10 16:59:32 +0300

    решение задачи CounterButtonComponent, MeetupPageComponent, MeetupsCalendar (02-components)

--- 02-components/03-MeetupsCalendar/MeetupsCalendar.js
+++ 02-components/03-MeetupsCalendar/MeetupsCalendar.js
@@ -9,34 +9,151 @@ export const MeetupsCalendar = {
     <div class="rangepicker__calendar">
       <div class="rangepicker__month-indicator">
         <div class="rangepicker__selector-controls">
-          <button class="rangepicker__selector-control-left"></button>
-          <div>Июнь 2020</div>
-          <button class="rangepicker__selector-control-right"></button>
+          <button class="rangepicker__selector-control-left" @click="prev"></button>
+          <div>{{ getLocaleMonthYear }}</div>
+          <button class="rangepicker__selector-control-right" @click="next"></button>
         </div>
       </div>
       <div class="rangepicker__date-grid">
-        <div class="rangepicker__cell rangepicker__cell_inactive">28</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">29</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">30</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">31</div>
-        <div class="rangepicker__cell">
-          1
-          <a class="rangepicker__event">Митап</a>
-          <a class="rangepicker__event">Митап</a>
+        <div class="rangepicker__cell rangepicker__cell_inactive" v-for="day in lastMonthDays">
+          {{day}}
         </div>
-        <div class="rangepicker__cell">2</div>
-        <div class="rangepicker__cell">3</div>
+        <div class="rangepicker__cell" v-for="day in getArrMonth.arrMonthISO">
+          {{new Date(day).getDate()}}
+          <a class="rangepicker__event" v-for="[item, title] in getMeetupsArr" v-if="day === item">
+            {{title}}
+          </a>
+        </div>
+        <div class="rangepicker__cell rangepicker__cell_inactive" v-for="day in nextMonthDays">
+          {{day}}
+        </div>
       </div>
     </div>
   </div>`,
 
   // Пропсы
+  props: {
+    meetups: {
+      type: Array,
+      required: true,
+    },
+  },
 
+  data() {
+    return {
+      date: new Date(),
+    };
+  },
   // В качестве локального состояния требуется хранить что-то,
   // что позволит определить текущий показывающийся месяц.
   // Изначально должен показываться текущий месяц
 
+  computed: {
+    currentYear() {
+      return this.date.getFullYear();
+    },
+    //текущий месяц
+    getLocaleMonthYear() {
+      return `${this.date.toLocaleString(navigator.language, {
+        month: 'long',
+      })} ${this.date.getFullYear()}`;
+    },
+
+    //последние дни предыдущего месяца
+    lastMonthDays() {
+      const arrLastMonth = [];
+      const lastDays = new Date(
+        this.currentYear,
+        this.date.getMonth(),
+        0,
+      ).getDate();
+      if (this.getDayWeekFirstDay > 0) {
+        for (
+          let i = lastDays - this.getDayWeekFirstDay + 1;
+          i <= lastDays;
+          i++
+        ) {
+          arrLastMonth.push(i);
+        }
+      }
+      return arrLastMonth;
+    },
+    //массив дней месяца
+    getArrMonth: function () {
+      //последний день текущего месяца
+      let lastDayMonth = new Date(
+        this.currentYear,
+        this.date.getMonth() + 1,
+        0,
+      ).getDate();
+
+      const arrMonth = Array.from({ length: lastDayMonth }, (day, i) => i + 1);
+      const arrMonthISO = [];
+      arrMonth.forEach((day) => {
+        day = new Date(this.currentYear, this.date.getMonth(), day + 1)
+          .toISOString()
+          .substr(0, 10);
+        arrMonthISO.push(day);
+      });
+
+      return {
+        arrMonthISO,
+        arrMonth,
+      };
+    },
+
+    //индекс дня недели первого дня месяца
+    getDayWeekFirstDay() {
+      //local week
+      return (
+        (new Date(this.currentYear, this.date.getMonth(), 1).getDay() + 6) % 7
+      );
+    },
+
+    //индекс дня недели последнего дня месяца
+    getDayWeekLastDay() {
+      //local week
+      return (
+        (new Date(
+          this.currentYear,
+          this.date.getMonth(),
+          this.getArrMonth.arrMonth.length,
+        ).getDay() + 6) % 7);
+    },
+
+    nextMonthDays() {
+      const nextDaysMonth = [];
+      if (this.getDayWeekLastDay < 6) {
+        for (let i = 1; i <= 6 - this.getDayWeekLastDay; i++) {
+          nextDaysMonth.push(i);
+        }
+      }
+      return nextDaysMonth;
+    },
+
+    getMeetupsArr() {
+      const meetupDays = [];
+      this.meetups.map((item) => {
+        const meetupDay = new Date(item.date).toISOString().substr(0, 10);
+        const title = item.title;
+        meetupDays.push([meetupDay, title]);
+      });
+      return meetupDays;
+    },
+
+    first() {
+      return new Date(this.currentYear, this.date.getMonth(), 1);
+    },
+  },
   // Вычислимые свойства помогут как с получением списка дней, так и с выводом информации
 
   // Методы понадобятся для переключения между месяцами
+  methods: {
+    prev() {
+      this.date = new Date(this.first.setMonth(this.first.getMonth() - 1));
+    },
+    next() {
+      this.date = new Date(this.first.setMonth(this.first.getMonth() + 1));
+    },
+  },
 };
