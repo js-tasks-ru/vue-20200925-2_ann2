3537727ebb8a79086e0fb1672449d07d1354bee8
Anna Podbelskaya <podag@yandex.ru>
2020-10-25 17:44:40 +0300

    решение 04-Layouts, 05-AuthPages (04-SPA)

--- 04-SPA/05-AuthPages/views/RegisterPage.vue
+++ 04-SPA/05-AuthPages/views/RegisterPage.vue
@@ -1,12 +1,86 @@
 <template>
-  <form class="form"></form>
+      <form class="form" @submit.prevent="sendForm">
+        <div class="form-group">
+          <label class="form-label">Email</label>
+          <div class="input-group">
+            <input type="email" class="form-control" v-model="email" />
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="form-label">Имя</label>
+          <div class="input-group">
+            <input type="text" class="form-control" v-model="fullname"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="form-label">Пароль</label>
+          <div class="input-group">
+            <input type="password" class="form-control" v-model="password"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="form-label">Повтор пароля</label>
+          <div class="input-group">
+            <input type="password" class="form-control" v-model="password2"/>
+          </div>
+        </div>
+        <div class="form-group">
+          <label class="checkbox"><input type="checkbox" v-model="agree"/> Я согласен с условиями <span></span></label>
+        </div>
+        <div class="form__buttons">
+          <button type="submit" class="button button_primary">Зарегистрироваться</button>
+        </div>
+        <div class="form__append">Уже есть аккаунт? <router-link :to="{name: 'login'}" class="link">Войдите</router-link></div>
+      </form>
 </template>
 
 <script>
-// import { register } from '../data';
+  import { register} from '../data';
 
 export default {
   name: 'RegisterPage',
+  data() {
+    return {
+    //  errors: [],
+      email: '',
+      fullname: '',
+      password: '',
+      password2: '',
+      agree: false,
+    }
+  },
+
+  methods: {
+    sendForm() {
+      if (!this.email) {
+        alert('Требуется ввести Email');
+        return;
+      }
+      if (!this.fullname) {
+        alert('Требуется ввести полное имя');
+        return;
+      }
+      if (!this.password) {
+        alert('Требуется ввести пароль');
+        return;
+      }
+      if (this.password !== this.password2) {
+        alert('Пароли не совпадают');
+        return;
+      }
+      if (!this.agree) {
+        alert('Требуется согласиться с условиями');
+        return;
+      }
+      register(this.email, this.fullname, this.password).then((res) => {
+        if (res.error) {
+          alert(res.message);
+        } else {
+          alert(res.id);
+        }
+      });
+    },
+  }
 };
 </script>
 
