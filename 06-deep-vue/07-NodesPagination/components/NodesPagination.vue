5f67a06b7d8b8971c539f09d42423e42a432553e
Anna Podbelskaya <podag@yandex.ru>
2021-03-12 19:33:02 +0300

    06-deep-vue, решение 07-NodesPagination

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,24 @@
 <script>
 export default {
   name: 'NodesPagination',
+  render(createElement) {
+    const start = (this.page - 1) * this.perPage;
+    let renderArr = [];
+    if (this.$slots.default) {
+      renderArr = this.$slots.default.slice(start, start + this.perPage);
+    }
+    return createElement('div', renderArr);
+  },
+  props: {
+    page: {
+      type: Number,
+      default: 0,
+    },
+    perPage: {
+      type: Number,
+      default: 0,
+    },
+  },
 };
 </script>
 
