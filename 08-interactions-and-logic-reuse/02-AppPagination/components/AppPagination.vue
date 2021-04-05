5a0dd5125d7a4e3f17ac554307628dc7a8ff87ca
Anna Podbelskaya <podag@yandex.ru>
2021-04-05 23:07:54 +0300

    08-interactions-and-logic-reuse решение AppPagination

--- 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
+++ 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
     <!-- Решение -->
+    <slot :item="item" v-for="item in renderItems"></slot>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AppPagination',
+  props: {
+    page: {
+      type: Number,
+      required: true,
+    },
+    perPage: {
+      type: Number,
+      default: 0,
+    },
+    items: Array,
+    default: [],
+  },
+  computed: {
+    renderItems() {
+      const start = (this.page - 1) * this.perPage;
+      return this.items.slice(start, start + this.perPage);
+    },
+  },
 };
 </script>
 
